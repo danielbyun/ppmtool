7d72107972e998c6cd8b3113fbc7df73d6c73bc0
danielbyun <dbyun92@gmail.com>
2019-08-07 15:44:44 +0900

    First react component

---
+++ ppmtool-react-client/src/Components/Dashboard.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Dashboard extends Component {
+  render() {
+    return <h1>Welcome to the damn Personal Project Tool Application</h1>;
+  }
+}
