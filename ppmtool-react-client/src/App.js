f4d0f938cb7c5a9d7c94243b5d421327dee95344
danielbyun <dbyun92@gmail.com>
2019-08-07 16:41:45 +0900

    Bringing in Bootstrap 4

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import Dashboard from "./Components/Dashboard";
 import Header from "./Components/Layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
