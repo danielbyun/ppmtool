4dc64b4fa435de33eda53be01d519d500fe55135
danielbyun <dbyun92@gmail.com>
2019-08-07 12:33:54 +0900

    Setup project model validation

--- ppmtool/src/main/java/org/danielbyun/ppmtool/model/Project.java
+++ ppmtool/src/main/java/org/danielbyun/ppmtool/model/Project.java
@@ -1,11 +1,14 @@
 package org.danielbyun.ppmtool.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Getter
@@ -18,13 +21,27 @@ public class Project {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Project Name is required")
     private String projectName;
+
+    @NotBlank(message = "Project Identifier is required")
+    @Size(min = 4, message = "Please write at least 4 characters")
+    @Column(updatable = false, unique = true)
     private String projectIdentifier;
+
+    @NotBlank(message = "Project Description is required")
     private String description;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date start_date;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date end_date;
 
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date created_at;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updated_at;
 
     @PrePersist
