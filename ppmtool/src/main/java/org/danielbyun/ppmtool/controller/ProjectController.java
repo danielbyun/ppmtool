c7dfc059349a81175223538aa77c4873ae642d1d
danielbyun <dbyun92@gmail.com>
2019-08-07 12:51:04 +0900

    Project Object validation part 1

--- ppmtool/src/main/java/org/danielbyun/ppmtool/controller/ProjectController.java
+++ ppmtool/src/main/java/org/danielbyun/ppmtool/controller/ProjectController.java
@@ -5,10 +5,13 @@ import org.danielbyun.ppmtool.model.Project;
 import org.danielbyun.ppmtool.service.ProjectService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @Slf4j
 @RestController
@@ -21,7 +24,10 @@ public class ProjectController {
     }
 
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
+        if (result.hasErrors()) {
+            return new ResponseEntity<>("Invalid Project Object", HttpStatus.BAD_REQUEST);
+        }
         log.info("new project: " + project.toString());
         projectService.saveOrUpdate(project);
         return new ResponseEntity<>(project, HttpStatus.CREATED);
