9325b22cbde2563579dd839e29c9c9aca3ac9753
danielbyun <dbyun92@gmail.com>
2019-08-07 17:03:17 +0900

    Style Dashboard, Navbar, ProjectItem

--- ppmtool-react-client/src/Components/Dashboard.js
+++ ppmtool-react-client/src/Components/Dashboard.js
@@ -5,8 +5,22 @@ export default class Dashboard extends Component {
   render() {
     return (
       <>
-        <h1>Welcome to the damn Personal Project Tool Application</h1>
-        <ProjectItem />
+        <div className="projects">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12">
+                <h1 className="display-4 text-center">Projects</h1>
+                <br />
+                <a href="ProjectForm.html" className="btn btn-lg btn-info">
+                  Create a Project
+                </a>
+                <br />
+                <hr />
+                <ProjectItem />
+              </div>
+            </div>
+          </div>
+        </div>
       </>
     );
   }
