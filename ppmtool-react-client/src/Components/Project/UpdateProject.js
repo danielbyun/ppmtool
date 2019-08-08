4338460417073d17417c456a6a8e23bf4eebc284
danielbyun <dbyun92@gmail.com>
2019-08-08 16:28:34 +0900

    Persist Project Object Update

--- ppmtool-react-client/src/Components/Project/UpdateProject.js
+++ ppmtool-react-client/src/Components/Project/UpdateProject.js
@@ -1,17 +1,79 @@
 import React, { Component } from "react";
-import { getProject } from "../../Actions/projectActions";
+import { getProject, createProject } from "../../Actions/projectActions";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import classnames from "classnames";
 
 class UpdateProject extends Component {
+  // set state
+  constructor() {
+    super();
+
+    this.state = {
+      id: "",
+      projectName: "",
+      projectIdentifier: "",
+      description: "",
+      start_date: "",
+      end_date: "",
+      errors: {}
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  // receiving props and updating the state to display as placeholders
+  componentWillReceiveProps(nextProps) {
+    const {
+      id,
+      projectName,
+      projectIdentifier,
+      description,
+      start_date,
+      end_date
+    } = nextProps.project;
+
+    this.setState({
+      id,
+      projectName,
+      projectIdentifier,
+      description,
+      start_date,
+      end_date
+    });
+  }
+
   componentDidMount() {
     // this id comes from the app.js
     const { id } = this.props.match.params;
     this.props.getProject(id, this.props.history);
   }
 
+  onChange(e) {
+    // reading the value of the each input
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  // form submit action
+  onSubmit(e) {
+    // to not have the webpage refresh
+    e.preventDefault();
+
+    // the form getting passed to the server
+    const updateProject = {
+      // get the value of each input
+      id: this.state.id,
+      projectName: this.state.projectName,
+      projectIdentifier: this.state.projectIdentifier,
+      description: this.state.description,
+      start_date: this.state.start_date,
+      end_date: this.state.end_date
+    };
+
+    this.props.createProject(updateProject, this.props.history);
+  }
+
   render() {
+    const { errors } = this.state;
     return (
       <div className="register">
         <div className="container">
@@ -19,26 +81,35 @@ class UpdateProject extends Component {
             <div className="col-md-8 m-auto">
               <h5 className="display-4 text-center">Update Project form</h5>
               <hr />
-              <form>
+              <form onSubmit={this.onSubmit}>
                 <div className="form-group">
                   <input
                     type="text"
                     className="form-control form-control-lg "
-                    placeholder="Project Name"
+                    placeholder={this.state.projectName}
+                    name="projectName"
+                    value={this.state.projectName}
+                    onChange={this.onChange}
                   />
                 </div>
                 <div className="form-group">
                   <input
                     type="text"
                     className="form-control form-control-lg"
-                    placeholder="Unique Project ID"
+                    placeholder={this.state.projectIdentifier}
+                    name="projectIdentifier"
+                    value={this.state.projectIdentifier}
+                    onChange={this.onChange}
                     disabled
                   />
                 </div>
                 <div className="form-group">
                   <textarea
                     className="form-control form-control-lg"
-                    placeholder="Project Description"
+                    placeholder={this.state.description}
+                    name="description"
+                    value={this.state.description}
+                    onChange={this.onChange}
                   />
                 </div>
                 <h6>Start Date</h6>
@@ -47,6 +118,8 @@ class UpdateProject extends Component {
                     type="date"
                     className="form-control form-control-lg"
                     name="start_date"
+                    value={this.state.start_date}
+                    onChange={this.onChange}
                   />
                 </div>
                 <h6>Estimated End Date</h6>
@@ -55,6 +128,8 @@ class UpdateProject extends Component {
                     type="date"
                     className="form-control form-control-lg"
                     name="end_date"
+                    value={this.state.end_date}
+                    onChange={this.onChange}
                   />
                 </div>
 
@@ -73,14 +148,17 @@ class UpdateProject extends Component {
 
 UpdateProject.propTypes = {
   getProject: PropTypes.func.isRequired,
-  project: PropTypes.object.isRequired
+  createProject: PropTypes.func.isRequired,
+  project: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
-  project: state.project.project
+  project: state.project.project,
+  errors: state.errors
 });
 
 export default connect(
   mapStateToProps,
-  { getProject }
+  { getProject, createProject }
 )(UpdateProject);
