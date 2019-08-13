16adeb70022e4237f09fcada28b60ae90434ce6e
danielbyun <dbyun92@gmail.com>
2019-08-13 16:35:00 +0900

    Lock operations to specific User (Update)

--- ppmtool/src/main/java/org/danielbyun/ppmtool/service/ProjectServiceImpl.java
+++ ppmtool/src/main/java/org/danielbyun/ppmtool/service/ProjectServiceImpl.java
@@ -28,6 +28,18 @@ public class ProjectServiceImpl implements ProjectService {
     @Override
     public Project saveOrUpdate(Project project, String username) {
         String identifier = project.getProjectIdentifier().toUpperCase();
+
+        if (project.getId() != null) {
+            Project existingProject = projectRepository.findByProjectIdentifier(project.getProjectIdentifier());
+
+            if (existingProject != null && (!existingProject.getProjectLeader().equals(username))) {
+                throw new ProjectNotFoundException("Project not found in your account.");
+            } else if (existingProject == null) {
+                throw new ProjectNotFoundException("Project with ID: '" + project.getProjectIdentifier() + "' cannot " +
+                        "be updated because it doesn't exist.");
+            }
+        }
+
         try {
             User user = userRepository.findByUsername(username);
             project.setUser(user);
